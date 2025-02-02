a65a161364d906943372287df8b46aa2a5662cfc
pierresqrt4761 <pierrehogenhaug@gmail.com>
2025-02-02 18:38:57 +0100

    add a basic /predict endpoint

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = FastAPI()
 def read_root():
     return {"message": "This is the starting point."}
 
+@app.post("/predict")
+def predict():
+    return {"generated_text": "This is a dummy response for now."}
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
